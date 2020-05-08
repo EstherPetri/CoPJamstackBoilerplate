e6de4c7fecdfdc6fb809654d27c4ac9d6ad3c534
EstherPetri <esther.petri@capgemini.com>
2020-05-08 16:11:03 +0200

    add multiple pages

--- src/scripts/sourceData.js
+++ src/scripts/sourceData.js
@@ -8,26 +8,29 @@ const client = contentful.createClient({
     accessToken: 'Ge5SVYPGqP04KDrjxFGUiJkaOFG-Tf4T58nV4VnSap4' // Insert Contentful Content Delivery API access token
 });
 
-client.getEntries({
-    'content_type': ['card', 'ImageQuestion'],
-})
+getData('quizQuestion');
+getData('results');
 
-.then(entries => {
-    const dir = './data';
-    const jsonData = entries.items;
-    for (var i = 0; i < jsonData.length; i++) {
-        console.log('Succesfully retrieved data entry: ' + entries.items[i].fields.title);
-    }
+function getData(contentType) {
+    client.getEntries({
+        'content_type': contentType,
+    }).then(entries => {
+        const dir = './data';
+        const jsonData = entries.items;
+        for (var i = 0; i < jsonData.length; i++) {
+            console.log('Succesfully retrieved data entry with type ' + contentType + ': ' + entries.items[i].fields.title);
+        }
 
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-        console.log('Not able to find data folder, therefore created this.');
-    } else {
-        console.log('data folder encountered, will therefore re-use this.');
-    }
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+            console.log('Not able to find data folder, therefore created this.');
+        } else {
+            console.log('data folder encountered, will therefore re-use this.');
+        }
 
-    fs.writeFile(dir + '/contentful.json', JSON.stringify(jsonData), function(err, result) {
-        if (err) console.log('error', err);
-        console.log('succesfully written data to file');
+        fs.writeFile(dir + '/' + contentType + '.json', JSON.stringify(jsonData), function(err, result) {
+            if (err) console.log('error', err);
+            console.log('succesfully written data to ' + contentType + 'file');
+        });
     });
-});
+}
