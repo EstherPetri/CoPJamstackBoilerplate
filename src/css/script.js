ee7cf5b4c23b6eba4b701d75795a1fad4c0a0f48
EstherPetri <esther.petri@capgemini.com>
2020-05-08 20:47:56 +0200

    fix score issue

--- src/css/script.js
+++ src/css/script.js
@@ -1,13 +1,18 @@
 function setPoints(){
     localStorage.setItem('points', 0);
+    alert(localStorage.getItem('points'));
 }
 
 function countingPoints(points, lastpage){    
     var point = Number(points);
     var old = Number(localStorage.getItem('points'));
     localStorage.setItem("points", old+point);
+    alert(localStorage.getItem('points'));
     if (lastpage == true){
+        alert(localStorage.getItem('points'));
+        console.log('true');
         var totalPoints = localStorage.getItem('points');
+        console.log(totalPoints);
         if(totalPoints < 7){
             window.location.href = "/results/3";
         }
@@ -15,13 +20,10 @@ function countingPoints(points, lastpage){
             window.location.href = "/results/1";
         }
         if(totalPoints < 13 && totalPoints > 9){
-            window.location.href = "/results/3";
+            window.location.href = "/results/4";
         }
         if(totalPoints > 12){
             window.location.href = "/results/2";
-        }
-        else{
-            window.location.href = "/results/";
         }
     }
 }
