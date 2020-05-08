a29cb4a30fd9554c85a18b8f9b936e0a2759c573
EstherPetri <esther.petri@capgemini.com>
2020-05-08 20:27:54 +0200

    add results

---
+++ src/css/script.js
@@ -0,0 +1,27 @@
+function setPoints(){
+    localStorage.setItem('points', 0);
+}
+
+function countingPoints(points, lastpage){    
+    var point = Number(points);
+    var old = Number(localStorage.getItem('points'));
+    localStorage.setItem("points", old+point);
+    if (lastpage == true){
+        var totalPoints = localStorage.getItem('points');
+        if(totalPoints < 7){
+            window.location.href = "/results/3";
+        }
+        if(totalPoints < 10 && totalPoints > 6){
+            window.location.href = "/results/1";
+        }
+        if(totalPoints < 13 && totalPoints > 9){
+            window.location.href = "/results/3";
+        }
+        if(totalPoints > 12){
+            window.location.href = "/results/2";
+        }
+        else{
+            window.location.href = "/results/";
+        }
+    }
+}
